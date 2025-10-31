814694fba5797312cdd7346d4df83d1c2f4bf25d
Lucero H <120441361+lucerohh@users.noreply.github.com>
2025-10-31 02:30:32 -0500

    add: create main test

---
+++ src/test/java/com/upc/dentify/demojenkinspipeline/DemojenkinspipelineApplicationTest.java
@@ -0,0 +1,14 @@
+package com.upc.dentify.demojenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DemojenkinspipelineApplicationTest {
+
+    @Test
+    public void testMessage() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
